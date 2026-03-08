276f71582d704b2df77c79f819494d4fcdbc660a
CamMlop31 <camila.mlop31@gmail.com>
2026-03-08 00:03:33 -0500

    Carga Inicial 2

--- app/src/main/java/com/example/taller_2/activities/Login.kt
+++ app/src/main/java/com/example/taller_2/activities/Login.kt
@@ -1,15 +1,26 @@
 package com.example.taller_2.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taller_2.R
-
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        val tv = findViewById<TextView>(R.id.linkRegistro)
+
+        tv.setOnClickListener {
+            val intent =Intent(this@Login, Registro::class.java)
+            startActivity(intent)
+            finish()
         }
+
     }
+}
